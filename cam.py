bd2d88609c1849e7e39098cf3a0d6968d845d1ce
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-12-09 22:13:57 +0100

    Route /image

--- cam.py
+++ cam.py
@@ -1,6 +1,9 @@
 import time
 import seeed_mlx9064x
-from flask import Flask, jsonify
+from flask import Flask, send_file
+from PIL import Image, ImageDraw
+import numpy as np
+import io
 import threading
 
 app = Flask(__name__)
@@ -8,24 +11,45 @@ frame = [0] * 768
 rounded = [round(num, 1) for num in frame]
 
 def sensor_data():
-    global frame, rounded
+    global frame
     mlx = seeed_mlx9064x.grove_mxl90640()
     while True:
         try:
             mlx.getFrame(frame)
-            rounded = [round(num, 1) for num in frame]
-            time.sleep(1)
+            time.sleep(0.1)
         except ValueError:
             continue
 
+def generate_image():
+    global frame
+    width, height = 32, 24
+    image = Image.new('RGB', (width, height))
+    draw = ImageDraw.Draw(image)
+
+    normalized_values = np.interp(frame, (min(frame), max(frame)), (0, 255))
+
+    for i, value in enumerate(normalized_values):
+        x = i % width
+        y = i // width
+        draw.rectangle([(x, y), (x + 1, y + 1)], fill=(int(value), 0, 255 - int(value)))
+
+    img_byte_array = io.BytesIO()
+    image.save(img_byte_array, format='JPEG')
+    img_byte_array.seek(0)
+    return img_byte_array
+
+@app.route('/image', methods=['GET'])
+def get_image():
+    return send_file(generate_image(), mimetype='image/jpeg')
+
 @app.route('/data', methods=['GET'])
 def get_data():
-    global rounded
-    return ' '.join(map(str, rounded))
+    global frame
+    return ' '.join(map(str, [round(num, 1) for num in frame]))
 
 if __name__ == '__main__':
     sensor_thread = threading.Thread(target=sensor_data)
     sensor_thread.daemon = True
     sensor_thread.start()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
 
